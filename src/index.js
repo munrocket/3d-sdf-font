e03655a523547bded2b00966eb571abb7e6ab276
munrocket <munrocket@pm.me>
2021-10-26 15:53:10 +0400

    fix bigger radius

--- src/index.js
+++ src/index.js
@@ -30,7 +30,10 @@ float sdCappedTorus(in vec3 p, in vec2 sc, in float ra, in float rb) {
 }
 
 float text(vec3 p) {
-  float d = 1e30;
+  float d = 1.30, fr = 1./sqrt(2.);
+  float rc = cos(2.2), rs = sin(2.2); mat2 rm = mat2(rc, -rs, rs, rc);
+  float gc = cos(1.25), gs = sin(1.25); mat2 gm = mat2(gc, -gs, gs, gc);
+  float jc = cos(0.687), js = sin(0.687); mat2 jm = mat2(jc, -js, js, jc);
   float scale = ${scale};
   vec3 translate = vec3(${tx}, ${ty}, ${tz});
   p = (p - translate) / scale;${sdf}
@@ -58,11 +61,11 @@ let charSdf = (c, px) => {
   case 'f': return `
   d = min(d, sdCapsule(p, vec3(${f32(.131+px)}, .035, .0), vec3(${f32(.131+px)}, .387, 0.), .035)); //f
   d = min(d, sdCapsule(p, vec3(${f32(.058+px)}, .319, .0), vec3(${f32(.229+px)}, .319, 0.), .035));
-  d = min(d, max(-p.y+.387, sdCappedTorus(vec3(p.y,-p.x,p.z) - vec3(.387, ${f32(-.239-px)}, 0.), normalize(vec2(.2,.04)), .108, .035)));`;
+  d = min(d, sdCappedTorus(vec3((p.x+p.y)*fr, (-p.x+p.y)*fr, p.z) - vec3(${f32((.387+.239+px))}*fr, ${f32(.387-.239-px)}*fr, 0.), vec2(fr, fr), .108, .035));`;
   case 'g': return `
   d = min(d, sdTorus(p.xzy - vec3(${f32(.196+px)}, 0., .177), vec2(.142, .035))); //g
   d = min(d, sdCapsule(p, vec3(${f32(.338+px)}, .0, .0), vec3(${f32(.338+px)}, .177, 0.), .035));
-  d = min(d, max(p.y, sdCappedTorus(vec3(-p.y, p.x, p.z) - vec3(0., ${f32(.196+px)}, 0.), vec2(sin(2.5), cos(2.5)), .142, .035)));`;
+  d = min(d, sdCappedTorus(vec3(vec2(-p.y, p.x)*gm, p.z) - vec3(vec2(0., ${f32(.196+px)})*gm, 0.), gm[1], .142, .035));`;
   case 'h': return `
   d = min(d, sdCapsule(p, vec3(${f32(.057+px)}, .035, 0.), vec3(${f32(.057+px)}, .494, 0.), .035)); //h
   d = min(d, sdCappedTorus(p - vec3(${f32(.177+px)}, .199, 0.), vec2(1.,0.), .120, .035));
@@ -73,17 +76,11 @@ let charSdf = (c, px) => {
   case 'j': return `
   d = min(d, sdCapsule(p, vec3(${f32(.057+px)}, 0., 0.), vec3(${f32(.057+px)}, .319, 0.), .035)); //j
   d = min(d, length(p - vec3(${f32(.057+px)}, .459, 0.)) - .035);
-  d = min(d, max(p.y, sdCappedTorus(vec3(-p.y, p.x, p.z) - vec3(0., ${f32(-.085+px)}, 0.), normalize(vec2(135.5,48.5)), .142, .035)));`;
+  d = min(d, sdCappedTorus(vec3(vec2(-p.y, p.x)*jm, p.z) - vec3(vec2(0., ${f32(-.085+px)})*jm, 0.), jm[1], .142, .035));`;
   case 'k': return `
   d = min(d, sdCapsule(p, vec3(${f32(.059+px)}, .035, .0), vec3(${f32(.059+px)}, .494, 0.), .035)); //k
   d = min(d, sdCapsule(p, vec3(${f32(.277+px)}, .319, .0), vec3(${f32(.06+px)}, .179, 0.), .035));
   d = min(d, sdCapsule(p, vec3(${f32(.277+px)}, .035, .0), vec3(mix(vec2(${f32(.277+px)}, .319), vec2(${f32(.06+px)}, .179), 0.72), 0.), .035));`;
-  case 'k2': return `
-  d = min(d, sdCapsule(p, vec3(${f32(-.452-px)}, .259, 0.), vec3(${f32(-.452-px)}, -.265, 0.), .035)); //k
-  d = min(d, sdCapsule(p, vec3(-.452, -.0645, 0.), vec3(-.388, -.0645, 0.), .035));
-  d = min(d, max(sdCappedTorus(vec3(p.x,-p.y,p.z) - vec3(-.388, -.1155, 0.), vec2(sin(1.45),cos(1.45)), .18, .035), -p.x-.388));
-  d = min(d, max(sdCappedTorus(p - vec3(-.370, -.244, 0.), vec2(1,0), .18, .035), -p.x-.388));
-  d = min(d, sdCapsule(p, vec3(-.191, -.244, 0.), vec3(-.191, -.265, 0.), .035));`;
   case 'l': return `
   d = min(d, sdCapsule(p, vec3(${f32(.057+px)}, .035, .0), vec3(${f32(.057+px)}, .494, 0.), .035)); //l`;
   case 'm': return `
@@ -106,11 +103,10 @@ let charSdf = (c, px) => {
   d = min(d, sdCapsule(p, vec3(${f32(.337+px)}, .177, .0), vec3(${f32(.337+px)}, -.177, 0.), .035));`;
   case 'r': return `
   d = min(d, sdCapsule(p, vec3(${f32(.053+px)}, .035, 0.), vec3(${f32(.053+px)}, .177, 0.), .035)); //r
-  d = min(d, max(-p.y+.177, sdCappedTorus(vec3(-p.y, -p.x, p.z) + vec3(.177, ${f32(.195+px)}, 0.), vec2(1, 0), .142, .035)));`;
+  d = min(d, sdCappedTorus(vec3((p.x+p.y)*fr, (-p.x+p.y)*fr, p.z) - vec3(${f32((.177+.195+px))}*fr, ${f32(.177-.195-px)}*fr, 0.), vec2(fr, fr), .142, .035));`;
   case 's': return `
-  float c = cos(2.2); float s = sin(2.2); mat2 sr = mat2(c, -s, s, c); //s
-  d = min(d, sdCappedTorus(vec3(-vec2(p.x, p.y)*sr,p.z) + vec3(vec2(${f32(.122+px)}, .248)*sr, 0.), sr[1], .071, .035));
-  d = min(d, sdCappedTorus(vec3(vec2(p.x, p.y)*sr,p.z) - vec3(vec2(${f32(.122+px)}, .106)*sr, 0.), sr[1], .071, .035));`;
+  d = min(d, sdCappedTorus(vec3(-vec2(p.x, p.y)*rm,p.z) + vec3(vec2(${f32(.122+px)}, .248)*rm, 0.), rm[1], .071, .035));//s
+  d = min(d, sdCappedTorus(vec3(vec2(p.x, p.y)*rm,p.z) - vec3(vec2(${f32(.122+px)}, .106)*rm, 0.), rm[1], .071, .035));`;
   case 't': return `
   d = min(d, sdCapsule(p, vec3(${f32(.118+px)}, .035, .0), vec3(${f32(.118+px)}, .494, 0.), .035)); //t
   d = min(d, sdCapsule(p, vec3(${f32(.032+px)}, .319, .0), vec3(${f32(.203+px)}, .319, 0.), .035));`;
@@ -137,7 +133,7 @@ let charSdf = (c, px) => {
   d = min(d, sdCapsule(p, vec3(${f32(.052+px)}, .141, .0), vec3(${f32(.052+px)}, .319, 0.), .035)); //y
   d = min(d, sdCappedTorus(vec3(p.x, -p.y, p.z) - vec3(${f32(.160+px)}, -.141, 0.), vec2(1,0), .108, .035));
   d = min(d, sdCapsule(p, vec3(${f32(.268+px)}, -.031, .0), vec3(${f32(.268+px)}, .319, 0.), .035));
-  d = min(d, max(p.y+.031, sdCappedTorus(vec3(-p.y, p.x, p.z) - vec3(.031, ${f32(.160+px)}, 0.), vec2(sin(2.5), cos(2.5)), .108, .035)));`;
+  d = min(d, sdCappedTorus(vec3(vec2(-p.y, p.x)*gm, p.z) - vec3(vec2(.031, ${f32(.160+px)})*gm, 0.), gm[1], .108, .035));`;
   case 'z': return `
   d = min(d, sdCapsule(p, vec3(${f32(.049+px)}, .319, .0), vec3(${f32(.270+px)}, .319, 0.), .035)); //z
   d = min(d, sdCapsule(p, vec3(${f32(.049+px)}, .035, .0), vec3(${f32(.270+px)}, .319, 0.), .035));
@@ -162,6 +158,7 @@ function sdfKerning(str) {
 function sdf(str, linespace = 1.15, scale, tx, ty, tz) {
   let sdf = '';
   let px = 0;
+  //str.replace('s', 'e');
   for(let i = 0; i < str.length; i++) {
     sdf += charSdf(str[i], px);
     if (i != str.length - 1) {
@@ -177,4 +174,5 @@ function sdf(str, linespace = 1.15, scale, tx, ty, tz) {
 }
 
 console.log(sdf(process.argv.slice(2).reduce((l, r) => l + ' ' + r, '')));
+//console.log(sdf('how vexingly\nquick daft\nzebras jump'));
 export { sdf };
